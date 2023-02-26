86ea09a1633e8ec6cbccaff0a58b1aa795c0a495
panduchanti <akuratipandurangarao@gmail.com>
2023-02-26 13:33:22 +0530

    resume

---
+++ src/components/Footer.js
@@ -0,0 +1,22 @@
+import { Container, Row, Col } from "react-bootstrap"
+import { MailchimpForm } from "./MailchimpForm"
+import logo from "../assets/img/logo.svg"
+
+export const Footer = () => {
+  return (
+    <footer className="footer">
+      <Container>
+        <Row className="align-items-center">
+          <MailchimpForm />
+          <Col size={12} sm={6}>
+            <img src={logo} alt="Logo" />
+          </Col>
+          <Col size={12} sm={6} className="text-center text-sm-end">
+            <p>Copyright 202#. All Rights Reserved</p>
+            <p>Pandu Chanti</p>
+          </Col>
+        </Row>
+      </Container>
+    </footer>
+  )
+}
